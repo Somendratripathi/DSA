bf2a649f9121431f7469a78eab907ee96218c52a
Somendra <somendra.dutt.tripathi@gmail.com>
2018-09-04 23:21:51 -0400

    dld

--- week2_algorithmic_warmup/4_least_common_multiple/lcm.py
+++ week2_algorithmic_warmup/4_least_common_multiple/lcm.py
@@ -1,4 +1,6 @@
 # Uses python3
+
+
 def gcd_efficient(a, b):
     if a%b==0 or b==1:
         return(b)
@@ -7,11 +9,20 @@ def gcd_efficient(a, b):
     
     
 def lcm_efficient(a,b):
-    return(a*b)/gcd_efficient(a, b)
+    return((float(a)/(gcd_efficient(a, b)))*b)
     
 if __name__ == "__main__":
     inp = input ()
     a, b = map(int, inp.split())
     if(b>a):
         b,a=a,b
-    print(int(lcm_efficient(a, b)))
+    if(a*b>10**10):
+        i = int((lcm_efficient(a, b)))+1000
+        while i:
+            if(i%a==0 and i%b==0):
+                print(i)
+                break
+            i=i-1
+    else:
+        print(int((lcm_efficient(a, b))))
+    
